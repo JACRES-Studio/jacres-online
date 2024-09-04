2e253719f753fe948f4a00f7f704a69e7fd9ccbb
JACRES-Studio <102634905+JACRES-Studio@users.noreply.github.com>
2024-09-04 11:02:46 +0800

    Create saga.js

---
+++ saga.js
@@ -0,0 +1,85 @@
+function l1_q1() {
+    document.querySelector("#question-1").innerHTML = "Perpendicular";
+    document.querySelector("#question-1").style.background = "green";
+    document.querySelector("#question-1").style.cursor = "default";
+}
+
+function l1_q2() {
+    document.querySelector("#question-2").innerHTML = "True";
+    document.querySelector("#question-2").style.background = "green";
+    document.querySelector("#question-2").style.cursor = "default";
+}
+
+function l1_q3() {
+    document.querySelector("#question-3").innerHTML = "Parabola";
+    document.querySelector("#question-3").style.background = "green";
+    document.querySelector("#question-3").style.cursor = "default";
+}
+
+function l1_q4() {
+    document.querySelector("#question-4").innerHTML = "True";
+    document.querySelector("#question-4").style.background = "green";
+    document.querySelector("#question-4").style.cursor = "default";
+}
+
+function l1_q5() {
+    document.querySelector("#question-5").innerHTML = "Two";
+    document.querySelector("#question-5").style.background = "green";
+    document.querySelector("#question-5").style.cursor = "default";
+}
+
+function l1_q6() {
+    document.querySelector("#question-6").innerHTML = "Parabola";
+    document.querySelector("#question-6").style.background = "green";
+    document.querySelector("#question-6").style.cursor = "default";
+}
+
+function l1_q7() {
+    document.querySelector("#question-7").innerHTML = "Hyperbola";
+    document.querySelector("#question-7").style.background = "green";
+    document.querySelector("#question-7").style.cursor = "default";
+}
+
+function l1_q8() {
+    document.querySelector("#question-8").innerHTML = "Circle";
+    document.querySelector("#question-8").style.background = "green";
+    document.querySelector("#question-8").style.cursor = "default";
+}
+
+function l1_q9() {
+    document.querySelector("#question-9").innerHTML = "Ellipse";
+    document.querySelector("#question-9").style.background = "green";
+    document.querySelector("#question-9").style.cursor = "default";
+}
+
+function l1_q10() {
+    document.querySelector("#question-10").innerHTML = "Hyperbola";
+    document.querySelector("#question-10").style.background = "green";
+    document.querySelector("#question-10").style.cursor = "default";
+}
+
+
+function l2_q1() {
+    document.querySelector("#l2_q1").innerHTML = "Circle";
+    document.querySelector("#l2_q1").style.background = "green";
+    document.querySelector("#l2_q1").style.cursor = "default";
+}
+
+function l2_q2() {
+    document.querySelector("#l2_q2").innerHTML = "Ellipse";
+    document.querySelector("#l2_q2").style.background = "green";
+    document.querySelector("#l2_q2").style.cursor = "default";
+}
+
+function l2_q3() {
+    document.querySelector("#l2_q3").innerHTML = "Hyperbola";
+    document.querySelector("#l2_q3").style.background = "green";
+    document.querySelector("#l2_q3").style.cursor = "default";
+}
+
+function l2_q4() {
+    document.querySelector("#l2_q4").innerHTML = "Parabola";
+    document.querySelector("#l2_q4").style.background = "green";
+    document.querySelector("#l2_q4").style.cursor = "default";
+}
+
