4dc208d18c3bf4ee7f70a71e102f8c32f3788405
JACRES-Studio <102634905+JACRES-Studio@users.noreply.github.com>
2024-09-04 12:57:51 +0800

    Update saga.js

--- saga.js
+++ saga.js
@@ -1,30 +1,35 @@
 function l1_q1() {
     document.querySelector("#question-1").innerHTML = "Perpendicular";
-    document.querySelector("#question-1").style.background = "green";
+    document.querySelector("#question-1").style.borderColor = "green";
+    document.querySelector("#question-1").style.color = "green";
     document.querySelector("#question-1").style.cursor = "default";
 }
 
 function l1_q2() {
     document.querySelector("#question-2").innerHTML = "True";
-    document.querySelector("#question-2").style.background = "green";
+    document.querySelector("#question-2").style.borderColor = "green";
+    document.querySelector("#question-2").style.color = "green";
     document.querySelector("#question-2").style.cursor = "default";
 }
 
 function l1_q3() {
     document.querySelector("#question-3").innerHTML = "Parabola";
-    document.querySelector("#question-3").style.background = "green";
+    document.querySelector("#question-3").style.borderColor = "green";
+    document.querySelector("#question-3").style.color = "green";
     document.querySelector("#question-3").style.cursor = "default";
 }
 
 function l1_q4() {
     document.querySelector("#question-4").innerHTML = "True";
-    document.querySelector("#question-4").style.background = "green";
+    document.querySelector("#question-4").style.borderColor = "green";
+    document.querySelector("#question-4").style.color = "green";
     document.querySelector("#question-4").style.cursor = "default";
 }
 
 function l1_q5() {
     document.querySelector("#question-5").innerHTML = "Two";
-    document.querySelector("#question-5").style.background = "green";
+    document.querySelector("#question-5").style.borderColor = "green";
+    document.querySelector("#question-5").style.color = "green";
     document.querySelector("#question-5").style.cursor = "default";
 }
 
@@ -36,85 +41,99 @@ function l1_q6() {
 
 function l1_q7() {
     document.querySelector("#question-7").innerHTML = "Hyperbola";
-    document.querySelector("#question-7").style.background = "green";
+    document.querySelector("#question-7").style.borderColor = "green";
+    document.querySelector("#question-7").style.color = "green";
     document.querySelector("#question-7").style.cursor = "default";
 }
 
 function l1_q8() {
     document.querySelector("#question-8").innerHTML = "Circle";
-    document.querySelector("#question-8").style.background = "green";
+    document.querySelector("#question-8").style.borderColor = "green";
+    document.querySelector("#question-8").style.color = "green";
     document.querySelector("#question-8").style.cursor = "default";
 }
 
 function l1_q9() {
     document.querySelector("#question-9").innerHTML = "Ellipse";
-    document.querySelector("#question-9").style.background = "green";
+    document.querySelector("#question-9").style.borderColor = "green";
+    document.querySelector("#question-9").style.color = "green";
     document.querySelector("#question-9").style.cursor = "default";
 }
 
 function l1_q10() {
     document.querySelector("#question-10").innerHTML = "Hyperbola";
-    document.querySelector("#question-10").style.background = "green";
+    document.querySelector("#question-10").style.borderColor = "green";
+    document.querySelector("#question-10").style.color = "green";
     document.querySelector("#question-10").style.cursor = "default";
 }
 
 
 function l2_q1() {
     document.querySelector("#l2_q1").innerHTML = "Circle";
-    document.querySelector("#l2_q1").style.background = "green";
+    document.querySelector("#l2_q1").style.borderColor = "green";
+    document.querySelector("#l2_q1").style.color = "green";
     document.querySelector("#l2_q1").style.cursor = "default";
 }
 
 function l2_q2() {
     document.querySelector("#l2_q2").innerHTML = "Ellipse";
-    document.querySelector("#l2_q2").style.background = "green";
+    document.querySelector("#l2_q2").style.borderColor = "green";
+    document.querySelector("#l2_q2").style.color = "green";
     document.querySelector("#l2_q2").style.cursor = "default";
 }
 
 function l2_q3() {
     document.querySelector("#l2_q3").innerHTML = "Hyperbola";
-    document.querySelector("#l2_q3").style.background = "green";
+    document.querySelector("#l2_q3").style.borderColor = "green";
+    document.querySelector("#l2_q3").style.color = "green";
     document.querySelector("#l2_q3").style.cursor = "default";
 }
 
 function l2_q4() {
     document.querySelector("#l2_q4").innerHTML = "Parabola";
-    document.querySelector("#l2_q4").style.background = "green";
+    document.querySelector("#l2_q4").style.borderColor = "green";
+    document.querySelector("#l2_q4").style.color = "green";
     document.querySelector("#l2_q4").style.cursor = "default";
 }
 
 function l3_q1() {
     document.querySelector("#l3_q1").innerHTML = "(x-1)²+(y-4)²=64 // C: (1,4) R=6";
-    document.querySelector("#l3_q1").style.background = "green";
+    document.querySelector("#l3_q1").style.borderColor = "green";
+    document.querySelector("#l3_q1").style.color = "green";
     document.querySelector("#l3_q1").style.cursor = "default";
 }
 
 function l3_q2() {
     document.querySelector("#l3_q2").innerHTML = "x+2)²+(y-2)²=36 // C: (-2,2) R=6";
-    document.querySelector("#l3_q2").style.background = "green";
+    document.querySelector("#l3_q2").style.borderColor = "green";
+    document.querySelector("#l3_q2").style.color = "green";
     document.querySelector("#l3_q2").style.cursor = "default";
 }
 
 function l3_q3() {
     document.querySelector("#l3_q3").innerHTML = "(x+5)²+(Y+2)² = 32 // C: (-5,-2) R= √32 or 5.7";
-    document.querySelector("#l3_q3").style.background = "green";
+    document.querySelector("#l3_q3").style.borderColor = "green";
+    document.querySelector("#l3_q3").style.color = "green";
     document.querySelector("#l3_q3").style.cursor = "default";
 }
 
 function l3_q4() {
     document.querySelector("#l3_q4").innerHTML = "x²+(y+4)² = 100 // C: (0,-4) R=10";
-    document.querySelector("#l3_q4").style.background = "green";
+    document.querySelector("#l3_q4").style.borderColor = "green";
+    document.querySelector("#l3_q4").style.color = "green";
     document.querySelector("#l3_q4").style.cursor = "default";
 }
 
 function l3_q5() {
     document.querySelector("#l3_q5").innerHTML = "(x-2)²+(y-0.4)²=5.7 // C: (2,0.4) R= 2.4 or √5.7";
-    document.querySelector("#l3_q5").style.background = "green";
+    document.querySelector("#l3_q5").style.borderColor = "green";
+    document.querySelector("#l3_q5").style.color = "green";
     document.querySelector("#l3_q5").style.cursor = "default";
 }
 
 function l3_q6() {
     document.querySelector("#l3_q6").innerHTML = "(x-2.5)²+(y+1.5)²=9 // C: (2.5 , 1.5) R=3";
-    document.querySelector("#l3_q6").style.background = "green";
+    document.querySelector("#l3_q6").style.borderColor = "green";
+    document.querySelector("#l3_q6").style.color = "green";
     document.querySelector("#l3_q6").style.cursor = "default";
 }
